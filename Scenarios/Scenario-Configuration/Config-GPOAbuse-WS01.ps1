f495d54461d33ce1cc42fbf887120ddac7fb71ab
hashf0rd <124741545+hashf0rd@users.noreply.github.com>
2025-03-13 22:44:56 +0000

    fixed up issue where dash character was copied in and was not recognised

--- Scenarios/Scenario-Configuration/Config-GPOAbuse-WS01.ps1
+++ Scenarios/Scenario-Configuration/Config-GPOAbuse-WS01.ps1
@@ -2,5 +2,5 @@
 Add-LocalGroupMember -Name "Remote Management Users" -Member "jim.duggan"
 
 # Install RSAT & GPO stuff
-Add-WindowsCapability –online –Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
 Add-WindowsCapability -Online -Name Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0
